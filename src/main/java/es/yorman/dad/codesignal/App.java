0f2a5d72a872d5780df40c5932364ef76bf664c7
Yorman <clase@yorman.es>
2023-09-15 13:57:46 +0100

    Primer commit

---
+++ src/main/java/es/yorman/dad/codesignal/App.java
@@ -0,0 +1,13 @@
+package es.yorman.dad.codesignal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
