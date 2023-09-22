543bf74271117259ca6c218eac5b5ed1f9d102ac
Yorman <yormansantos10@hotmail.com>
2023-09-22 12:52:13 +0100

    Se modifica CheckPalindrome

--- src/main/java/es/yorman/dad/codesignal/CheckPalindrome.java
+++ src/main/java/es/yorman/dad/codesignal/CheckPalindrome.java
@@ -4,13 +4,8 @@ public class CheckPalindrome {
 
 	boolean solution(String inputString) {
 	    String firstString = inputString.replaceAll(" ", "").toLowerCase();
-	    String secondString = "";
-	    
-	    for(int i = firstString.length() - 1; i >= 0; i--) {
-	        secondString += firstString.charAt(i);
-	    }
-	    
-	    return firstString.equals(secondString);
+
+	    return firstString.equals(new StringBuffer(firstString).reverse().toString());
 	}
 	
 }
